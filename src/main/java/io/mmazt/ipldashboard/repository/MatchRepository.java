1c0c67cfe5a0da39f73234fd13233a6be7696575
Mudassir Ali <alimudassir12@gmail.com>
2021-09-30 07:20:17 +0530

    Implement Match API

--- src/main/java/io/mmazt/ipldashboard/repository/MatchRepository.java
+++ src/main/java/io/mmazt/ipldashboard/repository/MatchRepository.java
@@ -1,14 +1,25 @@
 package io.mmazt.ipldashboard.repository;
 
 import io.mmazt.ipldashboard.model.Match;
+import org.apache.tomcat.jni.Local;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface MatchRepository extends CrudRepository<Match, Long> {
     List<Match> getByTeam1OrTeam2OrderByDateDesc(String teamName1, String teamName2, Pageable pageable);
+
+    @Query("select m from Match m where (m.team1 = :teamName or m.team2 = :teamName) and m.date between :dateStart and :dateEnd order by date desc")
+    List<Match> getMatchesByTeamBetweenDates(
+            @Param("teamName") String teamName,
+            @Param("dateStart") LocalDate startDate,
+            @Param("dateEnd") LocalDate endDate
+    );
 
     default List<Match> findLatestMatchesByTeam(String teamName, int count) {
         return getByTeam1OrTeam2OrderByDateDesc(teamName, teamName, PageRequest.of(0, count));
