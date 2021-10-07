e6f539591baa0e1391aacc5bcfa6e04cc7fb1059
Mudassir Ali <alimudassir12@gmail.com>
2021-10-07 07:53:23 +0530

    Setting Up Builds

--- src/frontend/src/pages/HomePage.js
+++ src/frontend/src/pages/HomePage.js
@@ -1,16 +1,14 @@
 import { React, useEffect, useState } from 'react';
 import { TeamTile } from '../components/TeamTile';
-import {useParams} from 'react-router-dom';
 import './HomePage.scss';
 
 export const HomePage = () => {
   const [teams, setTeams] = useState([]);
-  const { teamName } = useParams();
 
   useEffect(
     () => {
       const fetchTeams = async () => {
-        const response = await fetch(`http://localhost:8080/team`);
+        const response = await fetch(`${process.env.REACT_APP_API_ROOT_URL}/team`);
         const data = await response.json();
         setTeams(data);
       };
@@ -25,7 +23,7 @@ export const HomePage = () => {
         <h1 className="app-name">IPL Dashboard</h1>
       </div>
       <div className="team-grid">
-        {teams.map(team => <TeamTile teamName={team.teamName}></TeamTile>)}
+        {teams.map(team => <TeamTile key={team.id} teamName={team.teamName}></TeamTile>)}
       </div>
     </div>
     
